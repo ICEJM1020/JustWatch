modify bug in PD saccade angle computation

Extractor/Extractor.py
@@ -73,11 +73,6 @@ def extract_features(data, ball_data, player_box_data, dtw_mode="fast", scale_ra
         "attention_fea": attention_features
         }
 
-    # saccade_features = extract_saccade_features(match_rounds, data_df.loc[:, ["Screen.x", "Screen.y"]], video_id)
-    # trajectory_features = extract_trajectory(data_df.loc[:, ["Screen.x", "Screen.y"]], video_id, scale_to_percentage=True)
-
-    # return {**saccade_features, **trajectory_features}
-
 
 def modify_saccade_features_round(rounds:dict, data_df:pd.DataFrame, ball_data_df:pd.DataFrame):
     data_df["frame"] = data_df.index
@@ -111,8 +106,8 @@ def modify_features(data, ball_data, rounds, dist_th=10):
     for _round, _round_index in rounds.items():
         if _round_index:
             temp_df = data_df.loc[_round_index, :].copy()
-            temp_df["sub_x"] = temp_df["Screen.x"]-temp_df["Screen.x"].shift(-1)
-            temp_df["sub_y"] = temp_df["Screen.y"]-temp_df["Screen.y"].shift(-1)
+            # temp_df["sub_x"] = temp_df["Screen.x"]-temp_df["Screen.x"].shift(-1)
+            # temp_df["sub_y"] = temp_df["Screen.y"]-temp_df["Screen.y"].shift(-1)
             temp_df["sub_x"] = np.round(temp_df["Screen.x"]-temp_df["Screen.x"].shift(-1))
             temp_df["sub_y"] = np.round(temp_df["Screen.y"]-temp_df["Screen.y"].shift(-1))
             temp_indices = temp_df.index.to_list()

Extractor/SaccadeFeatures.py
@@ -149,8 +149,9 @@ def compute_saccade_path_lite(df:pd.DataFrame):
         row_1 = df.loc[indices[_i - 1], :]
         row_2 = df.loc[indices[_i], :]
 
-        _dist = np.sqrt((row_1["Screen.x"] - row_2["Screen.x"])**2 + (row_1["Screen.y"] - row_2["Screen.y"])**2)
-        _angle = np.arctan(_dist / VR_ZDIST) / np.pi * 180
+        # _dist = np.sqrt((row_1["Screen.x"] - row_2["Screen.x"])**2 + (row_1["Screen.y"] - row_2["Screen.y"])**2)
+        # _angle = np.arctan(_dist / VR_ZDIST) / np.pi * 180
+        _angle = convert_dist_angle(row_1["Screen.x"], row_1["Screen.y"], row_2["Screen.x"], row_2["Screen.y"])
 
         _angles.append(_angle)
         _speeds.append(np.divide(_angle, _dura))
@@ -188,8 +189,9 @@ def compute_saccade_path(df:pd.DataFrame, ball_data:pd.DataFrame):
         row_1 = df.loc[indices[_i - 1], :]
         row_2 = df.loc[indices[_i], :]
 
-        _dist = np.sqrt((row_1["Screen.x"] - row_2["Screen.x"])**2 + (row_1["Screen.y"] - row_2["Screen.y"])**2)
-        _angle = np.arctan(_dist / VR_ZDIST) / np.pi * 180
+        # _dist = np.sqrt((row_1["Screen.x"] - row_2["Screen.x"])**2 + (row_1["Screen.y"] - row_2["Screen.y"])**2)
+        # _angle = np.arctan(_dist / VR_ZDIST) / np.pi * 180
+        _angle = convert_dist_angle(row_1["Screen.x"], row_1["Screen.y"], row_2["Screen.x"], row_2["Screen.y"])
         _speeds.append(np.divide(_angle, _dura))
 
     _eye_dist = np.sqrt((df.iloc[0, :]["Screen.x"] - df.iloc[-1, :]["Screen.x"])**2 + (df.iloc[0, :]["Screen.y"] - df.iloc[-1, :]["Screen.y"])**2)